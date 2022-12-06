4f9a09d6f424601014f8357fc8dcb183f74b7eff
Anton Moberg <antmo@MacBook-Pro-3.local>
2022-12-06 23:08:13 +0100

    day 6

--- day6/part2.js
+++ day6/part2.js
@@ -1,5 +1,14 @@
 const fs = require('fs')
 
-let data = fs.readFileSync('./day4/input.txt', 'UTF-8').split(/\n/)
+let data = fs.readFileSync('./day6/input.txt', 'UTF-8').toString();
 
-console.log(data);
+let answer;
+for (let i = 13; i < data.length; i++) {
+    const chars = data.slice(i-13, i+1);
+    if (chars.length === new Set(chars).size) {
+        answer = i+1;
+        break;
+    }
+    
+}
+console.log(answer);
