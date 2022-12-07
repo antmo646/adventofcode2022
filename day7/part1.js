6b7443c3d9ee9615f140c3eebebba0608124bd5b
Moberg <Anton.Moberg@sgdsgruppen.se>
2022-12-07 16:44:26 +0100

    day 7

--- day7/part1.js
+++ day7/part1.js
@@ -1,5 +1,52 @@
 const fs = require('fs')
 
-let data = fs.readFileSync('./day4/input.txt', 'UTF-8').split(/\n/)
+let data = fs.readFileSync('./day7/input.txt', 'UTF-8').split(/\r\n/)
 
-console.log(data);
+let cwd = '';
+
+const fileSystem = new Map();
+
+
+
+data.forEach(row => {
+    const command = row.slice(0, 4).trim();
+    if (command === '$ cd') {
+        const dir = row.split(command)[1].trim();
+        if (dir === '..') cwd = cwd.slice(0, cwd.lastIndexOf('/'));
+        else if (dir === '/') cwd = dir;
+        else cwd = `${cwd}/${dir}`;
+    }
+    else if (command === '$ ls') {
+        if (!fileSystem.has(cwd)) fileSystem.set(cwd, []);
+    }
+    else if (command === 'dir') {
+        const fullDir = `${cwd}/${row.split(command)[1].trim()}`;
+        if (!fileSystem.has(fullDir)) fileSystem.set(fullDir, []);
+        fileSystem.get(cwd).push(fileSystem.get(fullDir));
+    } else {
+        const fileSize = row.split(' ')[0];
+        fileSystem.get(cwd).push(fileSize);
+    }
+});
+
+
+const calcDirSize = (arr) => {
+    let size = 0;
+    arr.forEach(item => {
+        if (Array.isArray(item)) size += calcDirSize(item);
+        else size += parseInt(item);
+        if (size > 100000) return;
+    })
+    return size;
+} 
+
+let answer = 0;
+for (item of fileSystem.values()) {
+    const size = calcDirSize(item);
+    if (size <= 100000) answer += size;
+};
+
+
+
+
+console.log(answer);
--- day7/part2.js
+++ day7/part2.js
@@ -1,5 +1,54 @@
 const fs = require('fs')
 
-let data = fs.readFileSync('./day4/input.txt', 'UTF-8').split(/\n/)
+let data = fs.readFileSync('./day7/input.txt', 'UTF-8').split(/\r\n/)
 
-console.log(data);
+
+const maxSize = 40000000;
+
+let cwd = '';
+
+const fileSystem = new Map();
+
+
+
+data.forEach(row => {
+    const command = row.slice(0, 4).trim();
+    if (command === '$ cd') {
+        const dir = row.split(command)[1].trim();
+        if (dir === '..') cwd = cwd.slice(0, cwd.lastIndexOf('/'));
+        else if (dir === '/') cwd = dir;
+        else cwd = `${cwd}/${dir}`;
+    }
+    else if (command === '$ ls') {
+        if (!fileSystem.has(cwd)) fileSystem.set(cwd, []);
+    }
+    else if (command === 'dir') {
+        const fullDir = `${cwd}/${row.split(command)[1].trim()}`;
+        if (!fileSystem.has(fullDir)) fileSystem.set(fullDir, []);
+        fileSystem.get(cwd).push(fileSystem.get(fullDir));
+    } else {
+        const fileSize = row.split(' ')[0];
+        fileSystem.get(cwd).push(fileSize);
+    }
+});
+
+
+const calcDirSize = (arr) => {
+    let size = 0;
+    arr.forEach(item => {
+        if (Array.isArray(item)) size += calcDirSize(item);
+        else size += parseInt(item);
+    })
+    return size;
+} 
+
+const totalUsed = calcDirSize(fileSystem.get('/'));
+const freeUp = totalUsed - maxSize;
+
+let answer = Number.MAX_SAFE_INTEGER;
+for (item of fileSystem.values()) {
+    const size = calcDirSize(item);
+    if (size > freeUp && size < answer) answer = size;
+};
+
+console.log(answer)
